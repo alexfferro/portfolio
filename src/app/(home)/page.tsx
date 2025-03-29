a135b8eb434cc3d5ced8c6a50f088d73a92ca262
Alex Ferro <alex.ferreira.ferro@gmail.com>
2025-03-29 12:40:57 -0300

    feat: insert samples of images

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,22 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <Image
+        alt="a young and beatitiful man, named Alex Ferreira"
+        priority
+        height={500}
+        width={500}
+        src="https://github.com/alexfferro.png"
+      />
+      <Image
+        alt="a man with a joystick, selecting a game character"
+        priority
+        height={500}
+        width={500}
+        src={"/home.svg"}
+      />
+    </div>
+  );
 }
